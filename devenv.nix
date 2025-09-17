fda68e4b0e87950cb60dda6728fafb9c1a94db86
Tomislav Maricevic <tomislav@tmarice.dev>
2025-09-17 15:46:10 +0200

    ts tools

--- devenv.nix
+++ devenv.nix
@@ -7,7 +7,18 @@
 }:
 
 {
-  packages = [ pkgs.git ];
+  packages = with pkgs; [
+    git
+    nodePackages.eslint_d
+    prettierd
+  ];
 
   languages.typescript.enable = true;
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_24;
+    corepack.enable = true;
+    pnpm.enable = true;
+  };
+
 }
